5b8da2d6e0d41cec1478635df7ee98b9cb2204ff
IRISUKANEZAKI <irisu0730@icloud.com>
2020-05-14 10:21:32 +0900

    スクリーンサイズの変更

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 // 定数
 //------------------------------------------------------------------
-#define SCREEN_SIZE_X 800		// 画面サイズ横
-#define SCREEN_SIZE_Y 600		// 画面サイズ縦
+#define SCREEN_SIZE_X 960		// 画面サイズ横
+#define SCREEN_SIZE_Y 750		// 画面サイズ縦
 
 
 
