da395094a121ce341e7453a5e136504465f696b5
Nakamura Ryosuke <remiriavamirion-114514@softbank.ne.jp>
2020-05-14 15:36:26 +0900

    shot.cppとshot.hを追加しました

---
+++ shot.cpp
@@ -0,0 +1,76 @@
+#include "DxLib.h"
+#include "main.h"
+#include "shot.h"
+
+
+//コンストラクタ
+Shot::Shot(const char image[])
+{
+	ShotImage = LoadGraph(image);
+	//ShotImage = LoadGraph("image/弾.png");
+	ShotPosX = 100;
+	ShotPosY = 100;
+	ShotSpeed = 15;
+	Shotflag = true;  //弾を打つとtrue
+}
+
+//デストラクタ
+Shot::~Shot()
+{
+
+}
+
+
+//プロトタイプ宣言
+void Shot::SystemInit(void)
+{
+
+}
+
+void Shot::GameInit(void)
+{
+
+}
+
+void Shot::Control(void)
+{
+
+}
+
+void Shot::Draw(void)
+{
+	if (Shotflag == true)
+	{
+		DrawGraph(ShotPosX, ShotPosY, ShotImage, true);
+	}
+}
+
+void Shot::CreateShot(XY pos)
+{
+	if (Shotflag == false)
+	{
+		Shotflag = true;
+		ShotPosX = pos.x;
+		ShotPosY = pos.y;
+	}
+}
+
+void Shot::DeleteShot(void)
+{
+
+}
+
+XY Shot::GetPos(void)
+{
+	return {ShotPosX, ShotPosY};
+}
+
+XY Shot::GetSize(void)
+{
+	return { SHOT_SIZE_X, SHOT_SIZE_Y };
+}
+
+bool Shot::visible(void)
+{
+	return Shotflag;
+}
---
+++ shot.h
@@ -0,0 +1,33 @@
+//#pragma once
+#define SHOT_SIZE_X 32
+#define SHOT_SIZE_Y 32
+
+#define SHOT_MAX 1
+
+class Shot
+{
+
+private:
+	//ÉÅÉìÉoïœêî
+	int ShotImage;
+	int ShotPosX;
+	int ShotPosY;
+	int Shotflag;
+	int ShotSpeed;
+public:
+	Shot(const char image[]);
+	~Shot();
+public:
+	void SystemInit(void);
+	void GameInit(void);
+	void Control(void);
+	void Draw(void);
+	
+	void CreateShot(XY pos);
+	void DeleteShot(void);
+
+	XY GetPos(void);
+	XY GetSize(void);
+	bool visible(void);
+
+};
