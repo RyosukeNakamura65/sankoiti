c055aded5f3d3985d19a8f971c93aefeaf93f152
IRISUKANEZAKI <irisu0730@icloud.com>
2020-05-15 13:06:37 +0900

    変更

--- stage.h
+++ stage.h
@@ -7,7 +7,7 @@
 #define MAP_X		 21
 #define MAP_Y		 15
 #define MAP_OFFSET_X 60
-#define MAP_OFFSET_Y 150
+#define MAP_OFFSET_Y 130
 
 
 // 列挙型
@@ -15,19 +15,19 @@
 // マップチップ管理用
 enum MAP_CHIP {
 	// ステージ１素材
-	S1_ROOF,
-	S1_WALL,
-	S1_ROAD1,
-	S1_UNBREAK_WALL,
-	S1_DESERT,
-	S1_ROAD2,
+	S1_ROOF,			// 屋根
+	S1_WALL,			// 壁
+	S1_ROAD,			// 道
+	S1_UNBREAK_WALL,	// 壊せない壁
+	S1_DESERT,			// 砂漠（床）
+	S1_KUSAMITI,		// 草道（床）
 
 	MAP_CHIP_MAX
 };
 
 // イベント管理用
 enum EVENT_ID {
-
+	EVENT_ID_MAX
 };
 
 
