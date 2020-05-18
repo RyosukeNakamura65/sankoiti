8917dbdfc5f5ce260c75d0b8f584d9e0e16acecd
IRISUKANEZAKI <irisu0730@icloud.com>
2020-05-18 15:51:55 +0900

    追加

--- player.h
+++ player.h
@@ -3,11 +3,6 @@
 #define PLAYER_DEF_SPEED 3
 #define PLAYER_ANI_MAX 4
 
-class Player
-{
-	// privateとして処理されている
-	char playerName;
-
 	enum DIR
 	{
 		DIR_UP,				// ﾌﾟﾚｲﾔｰの後ろ姿
@@ -17,41 +12,28 @@ class Player
 		DIR_MAX
 	};
 
-private:
-	const int START_POS_X;	// プレイヤーの初期位置X座標
-	const int START_POS_Y;	// プレイヤーの初期位置Y座標
+	enum CHAR_FILE
+	{
+		CHAR_FILE_FACE,		// ｷｬﾗの顔写真
+		CHAR_FILE_WALK,		// ｷｬﾗのｳｫｰｸｲﾒｰｼﾞ
+		CHAR_FILE_HIT,		// ｷｬﾗのﾋｯﾄｲﾒｰｼﾞ
+		CHAR_FILE_SHOT,		// ｷｬﾗのｼｮｯﾄｲﾒｰｼﾞ
+		CHAR_FILE_MAX
+	};
 
-	// メンバ変数
-	int playerImage[PLAYER_ANI_MAX][DIR_MAX];
-	int playerFaceImage;
-	int playerHitImage;
-	int playerShotImage[DIR_MAX];
-	XY playerPos;
-	int playerSpeed;
-	int playerCounter;
-	int playerDir;
-	bool playerFlag;
-	bool playerShotFlag;
-	bool playerMoveFlag;
+	struct CHAR_IMAGE
+	{
+		int faceImage;									// 顔ｲﾒｰｼﾞ
+		int walkImage[DIR_MAX][PLAYER_ANI_MAX];			// 全身ｲﾒｰｼﾞ
+		int hitImage;									// 移動ｲﾒｰｼﾞ
+		int shotImage;
+	};
 
 	int playerNo;
 	KEY_LIST keyList;
 
-public:
-	//Player(int no);
-	Player(int no, int posX, int posY, const char walkImage[], const char faceImage[]
-		, const char hitImage[], const char shotImage[], KEY_LIST key);
-	~Player();
-
-public:
-	// メンバ関数
-	void SystemInit(void);
-	void GameInit(void);
-	void Control(void);
-	void Draw(void);
-
-	XY GetPos(void);
-	XY GetSize(void);
-	
-	void DeletePlayer(void);
-};
+	// ﾌﾟﾛﾄﾀｲﾌﾟ宣言
+	void playerSystemInit(void);
+	void playerGameInit(void);
+	void playerControl(void);
+	void playerDraw(void);	
